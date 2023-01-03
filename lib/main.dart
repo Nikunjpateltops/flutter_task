8048f0ceb5532a9eaefa226c62773d7ec26ca7e5
Nikunjpateltops <121278713+Nikunjpateltops@users.noreply.github.com>
2023-01-03 22:50:04 +0530

    cafe_ordering_system

--- lib/main.dart
+++ lib/main.dart
@@ -1,208 +1,187 @@
 import 'package:flutter/material.dart';
-import 'package:login_form_task/forgot_pass.dart';
-import 'package:login_form_task/login_now.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
+  bool value = false;
+
+  MyApp({super.key});
+
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Login_Form',
-      home: login_form(),
+      title: 'Cafe_ordering_system',
+      home: cafe_ordering_system(),
     );
   }
 }
 
-class login_form extends StatefulWidget {
-  const login_form({Key? key}) : super(key: key);
+class cafe_ordering_system extends StatefulWidget {
+  const cafe_ordering_system({Key? key}) : super(key: key);
 
   @override
-  State<login_form> createState() => _login_formState();
+  State<cafe_ordering_system> createState() => _cafe_ordering_systemState();
 }
 
-class _login_formState extends State<login_form> {
-  final Email_controller = TextEditingController();
-  final Password_controller = TextEditingController();
+var amount = 0;
 
-  bool status = false;
+class _cafe_ordering_systemState extends State<cafe_ordering_system> {
+  var cheakbox1 = false;
+  var cheakbox2 = false;
+  var cheakbox3 = false;
+  var cheakbox4 = false;
+  var cheakbox5 = false;
 
+  var pizza = 870;
+  var burger = 320;
+  var coffe = 450;
+  var cake = 700;
+  var icecream = 400;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // shape: const OutlineInputBorder(
-        //     borderRadius: BorderRadius.only(
-        //       bottomLeft: Radius.circular(20),
-        //       bottomRight: Radius.circular(20),
-        //     ),
-        //     borderSide: BorderSide(width: 1)),
-        bottomOpacity: 0.5,
-        backgroundColor: Colors.green,
-        title: const Text('Login Form Task',
-            style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-                color: Colors.black)),
+        title: const Text('Cafe_ordering_system'),
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(26.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(children: const [
-                Text(
-                  'Welcome back',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 20),
+          padding: const EdgeInsets.all(15),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text(
+              'Cafe_Ordering_System',
+              style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.lightBlue),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: cheakbox1,
+                  onChanged: (value) {
+                    setState(() {
+                      cheakbox1 = value!;
+                      if (cheakbox1 == true) {
+                        amount += pizza;
+                      } else {
+                        amount -= pizza;
+                      }
+                    });
+                  },
                 ),
-              ]),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: const [
-                  Text(
-                    'Login to your account',
-                    style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w800,
-                        color: Colors.black),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  labelText: 'Email',
-                  counterText: null,
-                  hintText: 'Enter your email',
-                  // icon: const Icon(
-                  //   Icons.email_outlined,
-                  // ),
-                  //errorText: 'Please enter valid email',
-                  iconColor: Colors.black,
+                const Text(
+                  'pizza @ rs.870',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: cheakbox2,
+                  onChanged: (value) {
+                    setState(() {
+                      cheakbox2 = value!;
+                      if (cheakbox2 == true) {
+                        amount += burger;
+                      } else {
+                        amount -= burger;
+                      }
+                    });
+                  },
                 ),
-                controller: Email_controller,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextField(
-                  //   keyboardType: TextInputType.number,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    labelText: 'Password',
-                    counterText: null,
-                    hintText: 'Enter password',
-                    // icon: const Icon(
-                    //   Icons.password,
-                    // ),
-                    //errorText: 'Please enter valid email',
-                    iconColor: Colors.black,
-                  ),
-                  controller: Password_controller),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Checkbox(
-                    value: status,
-                    onChanged: (value) {
-                      setState(() {
-                        status = value!;
-                      });
-                    },
-                  ),
-                  const Text('Remember me',
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.black)),
-                  SizedBox(
-                    width: 45,
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => forgotpassword(),
-                            ));
-                      },
-                      child: const Text(
-                        'Forgot Password?',
-                        style: TextStyle(
-                            color: Colors.blue,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 18),
-                      )),
-                ],
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Container(
-                height: 60,
-                width: 300,
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(16)),
-                child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => login_now(),
-                          ));
-                    },
-                    child: const Text(
-                      'Login Now',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 20),
-                    )),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 60,
-                width: 300,
-                decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(16)),
-                child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => login_now(),
-                          ));
-                    },
-                    child: const Text(
-                      'Continue with Google',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 20),
-                    )),
-              )
-            ],
-          ),
+                const Text(
+                  'Burger @ rs.320',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: cheakbox3,
+                  onChanged: (value) {
+                    setState(() {
+                      cheakbox3 = value!;
+                      if (cheakbox3 == true) {
+                        amount += coffe;
+                      } else {
+                        amount -= coffe;
+                      }
+                    });
+                  },
+                ),
+                const Text(
+                  'Coffe @ rs.450',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: cheakbox4,
+                  onChanged: (value) {
+                    setState(() {
+                      cheakbox4 = value!;
+                      if (cheakbox4 == true) {
+                        amount += cake;
+                      } else {
+                        amount -= cake;
+                      }
+                    });
+                  },
+                ),
+                const Text(
+                  'cake @ rs.700',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: cheakbox5,
+                  onChanged: (value) {
+                    setState(() {
+                      cheakbox5 = value!;
+                      if (cheakbox5 == true) {
+                        amount += icecream;
+                      } else {
+                        amount -= icecream;
+                      }
+                    });
+                  },
+                ),
+                const Text(
+                  'Ice-cream @ rs.400',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print('your total bill is $amount');
+
+                },
+                child: const Text('Done Order')),
+            const SizedBox(
+              height: 30,
+            ),
+            Text('Your total bill amount  is ${amount}',
+                style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.blue)),
+
+          ]),
         ),
       ),
     );
